a8e797f60f41eac2214f8cae91bc375eb32e3159
mateeque39 <mateeque39@gmail.com>
2026-01-06 00:00:58 +0500

    Fix migrations to use public database URL

--- run-migrations.js
+++ run-migrations.js
@@ -4,7 +4,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_PUBLIC_URL || process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 async function runMigrations() {
